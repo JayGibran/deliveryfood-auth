59259edb5f09b22a77cf889398c98f74044ed51a
jaygibran <jay.gibran@sonalake.com>
2023-02-14 19:51:09 +0000

    Configured Authorization Server flow with Password Credentials and Opaque Tokens

--- src/main/java/com/jaygibran/deliveryfood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/jaygibran/deliveryfood/auth/AuthorizationServerConfig.java
@@ -1,12 +1,37 @@
 package com.jaygibran.deliveryfood.auth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter {
+    
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
+    public AuthorizationServerConfig(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
 
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                    .withClient("deliveryfood-web")
+                    .secret(passwordEncoder.encode("123"))
+                    .authorizedGrantTypes("password")
+                    .scopes("write", "read");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)  {
+        endpoints.authenticationManager(authenticationManager);
+    }
 }
