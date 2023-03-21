4262d3caba1322523f833270b28bbc8a623bd3c8
jaygibran <jay.gibran@sonalake.com>
2023-03-21 18:34:27 +0000

    Loading Oauth2 clients from database

--- src/main/java/com/jaygibran/deliveryfood/auth/core/AuthorizationServerConfig.java
+++ src/main/java/com/jaygibran/deliveryfood/auth/core/AuthorizationServerConfig.java
@@ -1,14 +1,13 @@
 package com.jaygibran.deliveryfood.auth.core;
 
-import com.jaygibran.deliveryfood.auth.core.JwtKeyStoreProperties;
-import com.jaygibran.deliveryfood.auth.core.PkceAuthorizationCodeTokenGranter;
+import java.util.Arrays;
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -23,57 +22,26 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
-
-import java.util.Arrays;
-
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter {
     
-    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
-    private final RedisConnectionFactory redisConnectionFactory;
+    private DataSource dataSource;
     
     private final JwtKeyStoreProperties jwtKeyStoreProperties;
     
-    public AuthorizationServerConfig(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, UserDetailsService userDetailsService, RedisConnectionFactory redisConnectionFactory, JwtKeyStoreProperties jwtKeyStoreProperties) {
-        this.passwordEncoder = passwordEncoder;
+    public AuthorizationServerConfig(AuthenticationManager authenticationManager, UserDetailsService userDetailsService, DataSource dataSource, JwtKeyStoreProperties jwtKeyStoreProperties) {
         this.authenticationManager = authenticationManager;
         this.userDetailsService = userDetailsService;
-        this.redisConnectionFactory = redisConnectionFactory;
+        this.dataSource = dataSource;
         this.jwtKeyStoreProperties = jwtKeyStoreProperties;
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-                .inMemory()
-                    .withClient("deliveryfood-web")
-                    .secret(passwordEncoder.encode("web123"))
-                    .authorizedGrantTypes("password", "refresh_token")
-                    .scopes("write", "read")
-                    .accessTokenValiditySeconds(6 * 60 * 60)
-                    .refreshTokenValiditySeconds(60 * 24 * 60 * 60)
-                .and()
-                    .withClient("webadmin")
-                    .authorizedGrantTypes("implicit")
-                    .scopes("write", "read")
-                    .redirectUris("http://client-application")
-                .and()
-                    .withClient("deliveryfoodanalytics")
-                    .secret(passwordEncoder.encode(""))
-                    .authorizedGrantTypes("authorization_code")
-                    .scopes("write", "read")
-                    .redirectUris("http://client-application")
-                .and()
-                    .withClient("billing")
-                    .secret(passwordEncoder.encode("billing123"))
-                    .authorizedGrantTypes("client_credentials")
-                    .scopes("write", "read")
-                .and()
-                    .withClient("checktoken")
-                    .secret(passwordEncoder.encode("check123"));
+        clients.jdbc(dataSource);
     }
 
     @Override
